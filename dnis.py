c24b619428ad5cdce1c04449688ed7c7347ea642
Sergio Manchon <sergio.manchon.manjon@gmail.com>
2023-10-29 04:19:59 +0100

    First commit

--- dnis.py
+++ dnis.py
@@ -1,18 +1,17 @@
-import random
+import random  # Importa el módulo random para generar números aleatorios.
 
 def generar_valor_unico(nombre):
-    random.seed(nombre)
+    random.seed(nombre)  # Establece la semilla del generador de números aleatorios.
 
+    numeros_aleatorios = ''.join(str(random.randint(0, 9)) for _ in range(8))  # Genera una cadena de 8 dígitos aleatorios.
 
-    numeros_aleatorios = ''.join(str(random.randint(0, 9)) for _ in range(8))
+    letra_aleatoria = chr(random.randint(ord('A'), ord('Z')))  # Genera una letra aleatoria en mayúsculas.
 
-    letra_aleatoria = chr(random.randint(ord('A'), ord('Z')))
+    valor_unico = numeros_aleatorios + letra_aleatoria  # Combina los dígitos y la letra aleatoria.
 
-    valor_unico = numeros_aleatorios + letra_aleatoria
-
-    return valor_unico
+    return valor_unico  # Devuelve el valor único generado.
 
 #nombre = "Ejemplo" 
 
-valor = generar_valor_unico(nombre = input("Indque el nombre: "))
-print("Valor generado:", valor)
+valor = generar_valor_unico(nombre = input("Indique el nombre: "))  # Llama a la función con el nombre ingresado por el usuario.
+print("Valor generado:", valor)  # Imprime el valor generado.
